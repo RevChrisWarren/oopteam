9c3bec906298cc4d918278382fb42daa70b5d983
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-04-19 09:49:19 -0500

    project ready to be submitted

--- src/generateTeam.js
+++ src/generateTeam.js
@@ -6,13 +6,15 @@ const generateManagerData = managerList => {
         ${managerList
             .map(({ name, id, email, officeNumber }) => {
                 return `
-                <div class="boxes">
-                    <h2 class="role">Manager</h2>
-                    <h3 class="employeeName">${name}</h3>
-                    <h3 class="employeeid">
-                    ID number: ${id}</h3>
+                    <div class="boxes">
+                    <h3 class="employeeName">${name}</h3>    
+                    <h2 class="role">&#9749 Manager</h2>
+                    <div class="info">
+                    <h4 class="employeeid">
+                    ID number: ${id}</h4>
                     <p>Email: </p><a href="mailto:${email}">${email}</a>
                     <p>${officeNumber}</p>
+                    </div>
                     </div>
                 `;
             })
@@ -25,13 +27,15 @@ const generateEngineerData = engineerList => {
         ${engineerList
             .map(({ name, id, email, github }) => {
                 return `
-                <div class="boxes">
-                    <h2 class="role">Engineer</h2>
+                    <div class="boxes engineer">
                     <h3 class="employeeName">${name}</h3>
-                    <h3 class="employeeid">
-                    ID number: ${id}</h3>
+                    <h2 class="role">&#128208 Engineer</h2>
+                    <div class="info">
+                    <h4 class="employeeid">
+                    ID number: ${id}</h4>
                     <p>Email: </p><a href="mailto:${email}">${email}</a>
-                    <p>Github: </p><a href="github.com/${github}">${github}</a>
+                    <p>Github: </p><a href="https://www.github.com/${github}">${github}</a>
+                    </div>
                     </div>
                 `;
             })
@@ -44,13 +48,15 @@ const generateInternData = internList => {
         ${internList
             .map(({ name, id, email, school }) => {
                 return `
-                <div class="boxes">
-                    <h2 class="role">Intern</h2>
+                    <div class="boxes intern">
                     <h3 class="employeeName">${name}</h3>
-                    <h3 class="employeeid">
-                    ID number: ${id}</h3>
+                    <h2 class="role">&#9999 Intern</h2>  
+                    <div class="info">                  
+                    <h4 class="employeeid">
+                    ID number: ${id}</h4>
                     <p>Email: </p><a href="mailto:${email}">${email}</a>
                     <p>${school}</p>
+                    </div>
                     </div>
                 `;
             })
@@ -59,10 +65,10 @@ const generateInternData = internList => {
         </section>
             `;
 }
-        const generateTeam = (managerList, engineerList, internList) => {
-            
-            
-                return `
+const generateTeam = (managerList, engineerList, internList) => {
+
+
+    return `
                 <!DOCTYPE html>
                 <html lang="en">
             
@@ -71,7 +77,6 @@ const generateInternData = internList => {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <meta http-equiv="X-UA-Compatible" content="ie=edge">
                     <title>Work Team Info</title>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
                     <link rel="stylesheet" href="./style.css">
                 </head>
             
@@ -92,6 +97,6 @@ const generateInternData = internList => {
                 </body>
                 </html>
                 `;
-            };
-        
-          module.exports = generateTeam;
+};
+
+module.exports = generateTeam;
